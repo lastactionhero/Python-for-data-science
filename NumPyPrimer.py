e8456bcdaca630d43fe9507d282c34927893eec3
Jitendra Patil (Tata Consultancy Services) <v-jipati@microsoft.com>
2020-07-05 23:26:09 -0700

    Added numpy exercise

---
+++ NumPyPrimer.py
@@ -0,0 +1,24 @@
+#%%
+import numpy as np
+#Array
+lst = list(range(0,10))
+ar = np.array(lst)
+print(ar)
+matr = np.array([[1,2,3],[11,22,33],[111,222,333]])
+print(matr)
+#use numpy functions 
+# %%
+np.arange(0,10)
+#%%
+np.zeros(4)
+#%%
+np.zeros((4,2))
+#%%
+np.ones((2,4))
+# %%
+np.linspace(1,5,5)
+# %%
+#identity matrix
+np.eye(10)
+
+# %%
